7ecba98c9e5b327d64841524eeececd9d15257fe
Matt Rogan <matt.rogan2@gmail.com>
2024-11-17 23:44:06 +0000

    SOLID refactors: remove logging for now, to implement later on.

--- src/app/Controllers/UserController.cs
+++ src/app/Controllers/UserController.cs
@@ -11,24 +11,20 @@ namespace SOLID_Principles.Controllers;
 /// <param name="service">DI User Service</param>
 /// <param name="logger">DI Logger</param>
 [ApiController]
-public class UserController(IUserService service, ILogger<UserController> logger) : ControllerBase
+[Route("api/[controller]")]
+public class UserController(IUserService service) : ControllerBase
 {
     private readonly IUserService _service = service;
-    private readonly ILogger<UserController> _logger = logger;
 
     /// <summary>
     /// Method to fetch all users
     /// </summary>
     /// <param name="token">Async Cancellation Token</param>
     /// <returns>An <see cref="IActionResult"/> containing the response to the request</returns>
-    [HttpGet("api/User")]
+    [HttpGet]
     public async Task<IActionResult> GetUsersAsync(CancellationToken token = default)
     {
-        _logger.LogInformation("Fetching users from database");
-
         var users = await _service.GetUsers(token);
-
-        _logger.LogInformation("Returning users");
 
         return Ok(users);
     }
@@ -39,32 +35,25 @@ public class UserController(IUserService service, ILogger<UserController> logger
     /// <param name="id">The unique identifier for the user to find</param>
     /// <param name="token">Async Cancellation Token</param>
     /// <returns>An <see cref="IActionResult"/> containing the response to the request</returns>
-    [HttpGet("api/User({id:int})")]
+    [HttpGet("{id:int}")]
     public async Task<IActionResult> GetUserAsync(int id, CancellationToken token = default)
     {
-        _logger.LogInformation("Fetching user with id {UserId} from database", id);
-
         var user = await _service.GetUser(id, token);
 
         if (user is null)
         {
-            _logger.LogInformation("User with id {UserId} couldn't be found", id);
             return NotFound();
         }
 
-        _logger.LogInformation("Returning user...");
-        
         return Ok(user);
     }
 
-    [HttpPost("api/User")]
+    [HttpPost]
     public async Task<IActionResult> PostUserAsync(PostUserDTO model, CancellationToken token = default)
     {
-        _logger.LogInformation("Attempting to create new user.");
 
         if (model is null || !ModelState.IsValid)
         {
-            _logger.LogInformation("Model was not valid.");
             return ValidationProblem(ModelState);
         }
 
@@ -72,14 +61,13 @@ public class UserController(IUserService service, ILogger<UserController> logger
 
         if (user is null)
         {
-            _logger.LogError("There was an error whilst creating the user.");
             return StatusCode((int)HttpStatusCode.ServiceUnavailable);
         }
 
         return Created(nameof(GetUserAsync), user);
     }
 
-    [HttpPut("api/User({id:int})")]
+    [HttpPut("{id:int}")]
     public async Task<IActionResult> PutUserAsync(int id, PostUserDTO model, CancellationToken token = default)
     {
         if (model is null || ModelState.IsValid == false)
@@ -102,15 +90,12 @@ public class UserController(IUserService service, ILogger<UserController> logger
         return Ok(user);
     }
 
-    [HttpDelete("api/User({id:int})")]
+    [HttpDelete("{id:int}")]
     public async Task<IActionResult> DeleteAsync(int id, CancellationToken token)
     {
-        _logger.LogInformation("Attempting to delete user with id {UserId}", id);
-
         var user = await _service.GetUser(id, token);
         if (user == null)
         {
-            _logger.LogInformation("User with id {UserId} not found", id);
             return NotFound();
         }
 
